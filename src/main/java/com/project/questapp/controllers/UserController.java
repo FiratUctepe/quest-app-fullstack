3567838f684282193e425a6c910bcc38af6026c8
Firat Uctepe <firat.uctepe1@gmail.com>
2023-03-17 15:17:57 +0300

    refreshed

--- src/main/java/com/project/questapp/controllers/UserController.java
+++ src/main/java/com/project/questapp/controllers/UserController.java
@@ -1,2 +1,45 @@
-package com.project.questapp.controllers;public class UserController {
+package com.project.questapp.controllers;
+
+import com.project.questapp.entities.User;
+import com.project.questapp.repos.UserRepository;
+import com.project.questapp.services.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    public UserRepository userRepository;
+    public UserService userService;
+
+    public UserController(UserRepository userRepository,UserService userService){
+        this.userRepository=userRepository;
+        this.userService=userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/{userId}")
+    public User getOneUser(@PathVariable Long userId){
+        return userRepository.findById(userId).orElse(null);
+    }
+
+    @PutMapping("/{userId}")
+    public User updateOneUser(@PathVariable Long userId,@RequestBody User newUser){
+        return userService.updateOneUser(userId,newUser);
+    }
+
+    @DeleteMapping("/{userId}")
+    public void deleteOneUser(@PathVariable Long userId){
+        userRepository.deleteById(userId);
+    }
 }
--- src/main/java/com/project/questapp/services/UserService.java
+++ src/main/java/com/project/questapp/services/UserService.java
@@ -1,2 +1,33 @@
-package com.project.questapp.services;public class UserService {
+package com.project.questapp.services;
+
+import com.project.questapp.entities.User;
+import com.project.questapp.repos.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    public UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository=userRepository;
+    }
+
+    public User updateOneUser(Long userId,User newUser){
+        User user = userRepository.findById(userId).get();
+
+        if(user != null){
+
+            user.setUserName(newUser.getUserName());
+            user.setPassword(newUser.getPassword());
+            user.setAvatar(newUser.getAvatar());
+
+            userRepository.save(user);
+            return user;
+        }else
+        return null;
+    }
+
 }
